61c250a0e118288aef00e34d6d9e55680ed10597
Franklin Jetty Johnson <77622937+jhonsfranky17@users.noreply.github.com>
2025-10-26 23:36:11 +0530

    Add delay in health check response
    Added a delay with a console log before sending the response.

--- controllers/health.ts
+++ controllers/health.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 export const checkHealth = (_: Request, res: Response) => {
+  setTimeout(()=>{console.log("Awaiting...")},30000);
   res.status(200).send({
     message: "Server Name: " + (process.env.SERVER_NAME || "Unknown"),
   });
