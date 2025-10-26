f00568be570daaa816784b153410cad99d2dbfe6
Franklin Jetty Johnson <77622937+jhonsfranky17@users.noreply.github.com>
2025-10-27 00:46:14 +0530

    Change server listen address to '0.0.0.0'

--- app.ts
+++ app.ts
@@ -11,7 +11,7 @@ const main = async () => {
 
   server.use("/health", healthRoutes);
 
-  server.listen(PORT, () => {
+  server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
